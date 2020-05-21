b204a8770033ebd75c7117145bedb2e7337a04fa
Toki Migimatsu <toki.migimatsu@gmail.com>
2020-05-21 16:54:01 -0700

    Add lesspipe

--- setup.sh
+++ setup.sh
@@ -4,13 +4,23 @@ set -e
 
 cd "$(dirname "$0")"
 git submodule init && git submodule update
+
+# 256 color
 tic -x xterm-256color-italic.terminfo
 tic -x tmux-256color.terminfo
+sudo cp lesspipe.git/lesspipe.sh lesspipe.git/code2color /usr/local/bin
+# pip3 install Pygments
+
+# Zsh
 mkdir -p .oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git .oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+# Tmux
 cd ~
 ln -s dotfiles/.tmux.conf .
 ln -s dotfiles/.tmux .
+
+# Vim
 ln -s dotfiles/.vimrc .
 if [[ -f .bashrc ]]; then
 	mv .bashrc .bashrc.bak
