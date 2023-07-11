701a2a007ca2b47541c1549babd56f8b42455f29
Toki Migimatsu <toki.migimatsu@gmail.com>
2023-07-11 13:15:00 -0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -5,23 +5,6 @@ set -e
 cd "$(dirname "$0")"
 git submodule init && git submodule update
 
-# 256 color
-tic -x xterm-256color-italic.terminfo
-tic -x tmux-256color.terminfo
-
-## nvim
-mkdir -p ../.config/nvim
-ln -s ../../dotfiles/init.vim ../.config/nvim/init.vim
-## MAC up to here.
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod +x nvim.appimage
-ln -s nvim.appimage nvim
-ln -s nvim.appimage vim
-curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-sudo cp lesspipe.git/lesspipe.sh lesspipe.git/code2color /usr/local/bin
-# pip3 install Pygments
-
 # Zsh
 mkdir -p .oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git .oh-my-zsh/custom/plugins/zsh-autosuggestions
@@ -45,17 +28,42 @@ if [[ -f .bashrc ]]; then
 fi
 ln -s dotfiles/.bashrc .
 
+## nvim
+# mkdir -p ../.config/nvim
+# ln -s ../../dotfiles/init.vim ../.config/nvim/init.vim
+
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+# chmod +x nvim.appimage
+# ln -s nvim.appimage nvim
+# ln -s nvim.appimage vim
+# curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# sudo apt install libfuse2
+
 # Vim
-ln -s dotfiles/.vimrc .
-mkdir .vim
-cd .vim
-ln -s ../dotfiles/.vim/autoload .
-ln -s ../dotfiles/.vim/colors .
-ln -s ../dotfiles/.vim/syntax .
-mkdir .backup
-mkdir .swp
-mkdir .undo
+# ln -s dotfiles/.vimrc .
+# mkdir .vim
+# cd .vim
+# ln -s ../dotfiles/.vim/autoload .
+# ln -s ../dotfiles/.vim/colors .
+# ln -s ../dotfiles/.vim/syntax .
+# mkdir .backup
+# mkdir .swp
+# mkdir .undo
 
 # Neovim
 # Install npm, go on linux
 # npm config set prefix ~/.local
+
+# 256 color
+#tic -x xterm-256color-italic.terminfo
+#tic -x tmux-256color.terminfo
+
+# sudo cp lesspipe.git/lesspipe.sh lesspipe.git/code2color /usr/local/bin
+# pip3 install Pygments
+
+sudo apt install clangd-13 clang-format-13 clang-tidy-13
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-13 13
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-13 13
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-13 13
+sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-13 13
+sudo update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-13 13
